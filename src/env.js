3b59349a0f7fb6e636cc3c49614acdd60faaa2ec
DiegoxK <diego.stv@outlook.es>
2025-05-12 17:45:50 -0500

    add MONGODB_URI to server and runtime environment variable schemas

--- src/env.js
+++ src/env.js
@@ -16,6 +16,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    MONGODB_URI: z.string(),
   },
 
   /**
@@ -36,6 +37,7 @@ export const env = createEnv({
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     NODE_ENV: process.env.NODE_ENV,
+    MONGODB_URI: process.env.MONGODB_URI,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**

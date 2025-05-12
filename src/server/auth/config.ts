5c1b21330021319f0d9190c0825855522ab84bc0
DiegoxK <diego.stv@outlook.es>
2025-05-12 17:56:44 -0500

    updated db client provider

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,7 +2,7 @@ import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import { clientPromise } from "@/server/db";
+import client from "@/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -31,7 +31,7 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter(client),
   providers: [
     DiscordProvider,
     /**
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,11 +1,31 @@
 import { env } from "@/env";
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const uri = env.MONGODB_URI;
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+};
 
-if (!uri) throw new Error("MONGODB_URI not set in environment variables");
+let client: MongoClient;
 
-const client = new MongoClient(uri);
-const clientPromise = client.connect();
+if (env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  const globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
 
-export { clientPromise };
+  globalWithMongo._mongoClient ??= new MongoClient(uri, options);
+  client = globalWithMongo._mongoClient;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+}
+
+// Export a module-scoped MongoClient. By doing this in a
+// separate module, the client can be shared across functions.
+export default client;
